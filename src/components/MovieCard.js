a1e64ec10951d66130ec09226485015850388fed
Chief <brianmwangi10808@gmail.com>
2024-03-24 03:09:30 -0400

    completed

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,17 +1,24 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 function MovieCard({
   title,
   posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
-  genres,
+  genres = [],
 }) {
   return (
     <div className="movie-card">
-      <img src={posterSrc} alt={title} />
+      <img src={posterSrc} alt={`Poster of the movie titled ${title}`} />
       <h2>{title}</h2>
       <small>{genres.join(", ")}</small>
     </div>
   );
 }
+
+MovieCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  posterSrc: PropTypes.string,
+  genres: PropTypes.arrayOf(PropTypes.string),
+};
 
 export default MovieCard;
